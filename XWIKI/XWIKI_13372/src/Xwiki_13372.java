43666c547fef913e78ef1318b5841c0dc2a429eb
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-08-21 10:20:55 +0200

    Clean the generated test for XXWIKI-13372

--- XWIKI/XWIKI_13372/src/Xwiki_13372.java
+++ XWIKI/XWIKI_13372/src/Xwiki_13372.java
@@ -1,9 +1,14 @@
 import com.xpn.xwiki.objects.BaseObject;
 import org.junit.Test;
-
+// The generated stack trace is:
+// java.lang.NullPointerException
+// 	at com.xpn.xwiki.objects.BaseProperty.equals(BaseProperty.java:96)
+// 	at com.xpn.xwiki.objects.BaseStringProperty.equals(BaseStringProperty.java:57)
+// 	at com.xpn.xwiki.objects.BaseCollection.equals(BaseCollection.java:614)
+// 	at com.xpn.xwiki.objects.BaseObject.equals(BaseObject.java:235)
 public class Xwiki_13372 {
     @Test
-    public void frame3() throws Throwable {
+    public void frame4() throws Throwable {
         BaseObject baseObject0 = new BaseObject();
         baseObject0.setLargeStringValue("", "");
         BaseObject baseObject1 = new BaseObject();
