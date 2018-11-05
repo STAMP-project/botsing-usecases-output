ff62c5d3446ef8648ddfa045d0347e0f734c4161
Jesús Gorroñogoitia <jesus.gorronogoitia@atos.net>
2018-11-05 11:00:33 +0100

    Initial Botsing experiments with IF crash exceptions

---
+++ Atos/Supersede/IF/botsing/run_botsing.sh
@@ -0,0 +1,1 @@
+java -Dis.admin.user=admin -Dis.admin.passwd=$2pRSid# -jar botsing-reproduction-1.0.0.jar -crash_log ./logs/exception14.log -target_frame 2 -projectCP ./libs
---
+++ Atos/Supersede/IF/botsing/run_botsing_exception14.sh
@@ -0,0 +1,1 @@
+java -jar botsing-reproduction-1.0.0.jar -crash_log ./logs/exception14b.log -target_frame 2 -projectCP ./libs
