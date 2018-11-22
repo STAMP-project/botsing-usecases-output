6129f2f92fb9942e80860e90e70bf98116de3255
Jesús Gorroñogoitia <jesus.gorronogoitia@atos.net>
2018-11-22 12:40:06 +0100

    Botsing tested in additional Atos IF exceptions

---
+++ Atos/Supersede/IF/botsing/run_botsing_exception1.sh
@@ -0,0 +1,1 @@
+java -jar botsing-reproduction-1.0.1.jar -crash_log ./logs/exception1.log -target_frame 3 -projectCP ./libs
---
+++ Atos/Supersede/IF/botsing/run_botsing_exception15.sh
@@ -0,0 +1,1 @@
+java -jar botsing-reproduction-1.0.1.jar -crash_log ./logs/exception15.log -target_frame 7 -projectCP ./libs
